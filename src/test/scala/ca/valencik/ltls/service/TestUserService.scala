a13d889425cb9c7997d1730dec183471bfaf30aa
Andrew Valencik <andrew.valencik@gmail.com>
2018-04-16 20:00:19 -0400

    Add explicit types to public methods

--- src/test/scala/ca/valencik/ltls/service/TestUserService.scala
+++ src/test/scala/ca/valencik/ltls/service/TestUserService.scala
@@ -2,17 +2,17 @@ package ca.valencik.ltls.service
 
 import cats.effect.IO
 import ca.valencik.ltls.TestUsers.users
-import ca.valencik.ltls.model.UserName
+import ca.valencik.ltls.model.{UserName, User}
 import ca.valencik.ltls.repository.algebra.UserRepository
 
 object TestUserService {
 
   class TestUserRepo extends UserRepository[IO] {
-    override def findUser(username: UserName) = IO {
+    override def findUser(username: UserName): IO[Option[User]] = IO {
       users.find(_.username.value == username.value)
     }
 
-    override def findAll = IO {
+    override def findAll: IO[Option[List[User]]] = IO {
       Some(users)
     }
 
